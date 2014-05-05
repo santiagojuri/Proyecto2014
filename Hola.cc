5ac3a8265e6155c058a69c5614eaa0eea4d3881e
santiagojuri <santiagojuri83@gmail.com>
2014-05-05 17:09:48 -0500

    Hola
    saludo a todos

---
+++ Hola.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Hola, Bienvenidos al Proyecto [nombre por poner] 2014 :D"<<endl;
+	return 0;
+}
+
