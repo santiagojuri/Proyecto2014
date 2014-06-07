8fc6a09fa8a6e7f2bb89df51d24212032dbe4671
Jessxcm <calajess@gmail.com>
2014-06-07 13:26:33 -0500

    Update clase_question.h

--- clase_question.h
+++ clase_question.h
@@ -1,16 +1,19 @@
-#ifndef QUESTION
-#define QUESTION
 #include "item.cc"
 
-class clase_question
+/*Clase para hacer una pregunta*/
+class Question
 {
-public:
-	clase_item& item;
-	std::vector<item> v;
+	/*Atributos privados o publicos?? */
+	std::vector<item> Objetos;
+	int level;
 
-	int level;		
+	/*Metodos*/
+public: 
+	/*Constructores*/
+	virtual void get_items(item); //1.constructor: se va agregando un item por item (especifico)
+	virtual void get_items(); //2.constructor: se crea todas las preguntas de forma random
+	virtual void get_items(int nitems); //3. constructor: de acuerdo a la cantidad de items se crean.
+	virtual void Remove_items(item); //quitar un item ()
 };
 
 
-
-#endif
