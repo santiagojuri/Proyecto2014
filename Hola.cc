9bd102aa03cd97471adfbf2c1cf38786e1f7d153
JDRV <J-sus.DRV@live.com>
2014-06-07 11:19:35 -0500

    Update Hola.cc

--- Hola.cc
+++ Hola.cc
@@ -1,7 +1,28 @@
-#include <iostream>
-#include <stdlib.h>
 
+#include <stdlib.h>
+#ifndef JUGADOR
+#define JUGADOR
+#include <ioestrem>
+#include "item.h"
+#include "clase_juego.h"
 using namespace std;
+
+class clase_jugador
+{
+
+	clase_juego& _ref_juego;
+	item& _ref_item;
+
+	vector<item> Item_select;
+
+
+};
+
+
+
+
+#endif
+
 
 int main(int argc, char const *argv[])
 {
