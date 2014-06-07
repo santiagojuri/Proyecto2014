c094130613fc0d27b3fba698128222803793ba5b
Jessxcm <calajess@gmail.com>
2014-06-07 17:38:23 -0500

    Update Pregunta.hpp

--- Pregunta.hpp
+++ Pregunta.hpp
@@ -26,3 +26,11 @@ public:
 	~Question(); //destructor
 
 };
+
+
+/*documenta
+	virtual void get_items(item); //1.constructor: se va agregando un item por item (especifico)
+	virtual void get_items(); //2.constructor: se crea todas las preguntas de forma random
+	virtual void get_items(int nitems); //3. constructor: de acuerdo a la cantidad de items se crean.
+	virtual void Remove_items(item); //quitar un item ()
+*/
