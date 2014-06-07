a19e65ed5dd75513df052fb3e698d37fde7f1b43
JDRV <J-sus.DRV@live.com>
2014-06-07 16:04:02 -0500

    Update clase_resultado.h

--- clase_resultado.h
+++ clase_resultado.h
@@ -12,19 +12,16 @@ public:
 
 	//metodos
 	reso(int tiempo1, int puntaje1);
-	print(int tiempo1, int puntaje1);
+	
 	
 };
 
 resultado::reso(int tiempo1, int puntaje1){
 	tiempo = tiempo1;
 	puntaje = puntaje;
-
-}
-
-resultado::print(int tiempo1, int puntaje1){
-	tiempo = tiempo1;
-	puntaje = puntaje1;
-
+	
 	cout<<"Tu resultado es en tiempo:"<<tiempo<< "Y" <<puntaje;
+
 }
+
+
