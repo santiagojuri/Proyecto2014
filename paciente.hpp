a98e9b24ed7d2fe68cff00e067f7e601c2d43036
Jessxcm <calajess@gmail.com>
2014-06-08 14:43:37 -0500

    Update paciente.hpp

--- paciente.hpp
+++ paciente.hpp
@@ -3,16 +3,15 @@
 
 /*Clase que trabaja con los datos de la persona o paciente*/
 class paciente{
+	std::string name;
+	int codigo;
+	int edad;
 
-private:
-
-  std::string nombre;
-  int codigo;
-  int edad;
 public:
+	paciente(std::string name,int cd,int age){
+		nombre=name;
+		codigo=cd;
+		edad=age;
+	}
 
-  paciente(std::string name,int cd, int age){
-    
-  }
 };
-  
